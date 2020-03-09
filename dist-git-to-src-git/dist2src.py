0ef0f4d674abf7ba5c8ab4f5f236db6dc9b59ae5
Hunor Csomortáni <csomh@redhat.com>
2020-03-09 13:51:59 +0100

    Document dist2src
    Write docstring for each subcommand and fix most of the TODOs.
    
    Add more logging.
    
    Signed-off-by: Hunor Csomortáni <csomh@redhat.com>

--- dist-git-to-src-git/dist2src.py
+++ dist-git-to-src-git/dist2src.py
@@ -1,20 +1,54 @@
 #!/usr/bin/python3
 
+import functools
+import logging
 import os
-import tarfile
+import re
+import shutil
+import tempfile
+
 import click
 import git
-import logging
 import sh
-from time import sleep
+from packit.config.package_config import get_local_specfile_path
+from rebasehelper.specfile import SpecFile
 
 
 logger = logging.getLogger(__name__)
 
 
 @click.group("dist2src")
-@click.option("-v", "--verbose", count=True)
+@click.option(
+    "-v", "--verbose", count=True, help="Increase verbosity. Repeat to log more."
+)
 def cli(verbose):
+    """Script to convert the tip of a branch from a dist-git repository
+    into a commit on a branch in a source-git repository.
+
+    As of now, this downloads the sources from the lookaside cache,
+    unpacks them, applies all the patches, and remove the applied patches
+    from the SPEC-file.
+
+    For example to convert git.centos.org/rpms/rpm, branch 'c8s', to a
+    source-git repo, with a branch also called 'c8s', in one step:
+
+        \b
+        $ cd git.centos.org
+        $ dist2src convert rpms/rpm:c8s src/rpm:c8s
+
+    For the same, but doing each conversion step separately:
+
+        \b
+        $ cd git.centos.org
+        $ dist2src checkout rpms/rpm c8s
+        $ dist2src get-archive rpms/rpm
+        $ dist2src checkout --orphan src/rpm c8s
+        $ dist2src extract-archive rpms/rpm src/rpm
+        $ dist2src copy-spec rpms/rpm src/rpm
+        $ dist2src copy-patches rpms/rpm src/rpm
+        $ dist2src apply-patches src/rpm
+        $ dist2src commit src/rpm
+    """
     logger.addHandler(logging.StreamHandler())
     if verbose > 1:
         logger.setLevel(logging.DEBUG)
@@ -22,11 +56,35 @@ def cli(verbose):
         logger.setLevel(logging.INFO)
 
 
+def log_call(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        args_string = ", ".join([repr(a) for a in args])
+        kwargs_string = ", ".join([f"{k}={v!r}" for k, v in kwargs.items()])
+        sep = ", " if args_string and kwargs_string else ""
+        logger.debug(f"{func.__name__}({args_string}{sep}{kwargs_string})")
+        ret = func(*args, **kwargs)
+        return ret
+    return wrapper
+
+
 @cli.command()
 @click.argument("path")
 @click.argument("branch")
-@click.option("--orphan", is_flag=True, help="Create an branch with disconnected history.")
+@click.option(
+    "--orphan", is_flag=True, help="Create an branch with disconnected history."
+)
+@log_call
 def checkout(path, branch, orphan=False):
+    """Checkout a Git repository.
+
+    This will create the directory at PATH, if it doesn't exist already,
+    and initialize it as a Git repository. The later is not destructive
+    in an existing directory.
+
+    Checking out BRANCH is done with the `-B` flag, which means the
+    branch is created if it doesn't exist or reset, if it does.
+    """
     if path.startswith(os.pardir):
         raise ValueError("This is bad, don't start path with '..'")
 
@@ -44,68 +102,112 @@ def checkout(path, branch, orphan=False):
 
 
 @cli.command()
-@click.argument("path")
-def get_archive(path):
-    # TODO(csomh): make the path to get_sources.sh configurable
-    get_sources = sh.Command("get_sources.sh")
-    with sh.pushd(path):
-        get_sources()
+@click.argument("gitdir")
+@log_call
+def get_archive(gitdir):
+    """Calls get_sources.sh in GITDIR.
+
+    GITDIR needs to be a dist-git repository.
+
+    Set DIST2SRC_GET_SOURCES to the path to git_sources.sh, if it's not
+    in the PATH.
+    """
+    script = os.getenv("DIST2SRC_GET_SOURCES", "get_sources.sh")
+    command = sh.Command(script)
+
+    with sh.pushd(gitdir):
+        stdout = command()
+
+    return stdout
 
 
 @cli.command()
 @click.argument("origin")
 @click.argument("dest")
+@log_call
 @click.pass_context
 def copy_spec(ctx, origin, dest):
-    import shutil
-    shutil.copytree(os.path.join(origin, "SPECS"), os.path.join(dest, "centos-packaging", "SPECS"))
+    """Copy the 'SPECS' directory from a dist-git repo to a source-git repo.
+
+    In the source-git repo this is going to be under 'centos-packaging'.
+
+    After copying stage the changes in DEST.
+    """
+    shutil.copytree(
+        os.path.join(origin, "SPECS"), os.path.join(dest, "centos-packaging", "SPECS")
+    )
     ctx.invoke(stage, gitdir=dest)
 
 
 @cli.command()
 @click.argument("origin")
 @click.argument("dest")
+@log_call
 @click.pass_context
 def copy_patches(ctx, origin, dest):
-    import shutil
-    files = filter(lambda x: x.endswith(".patch"), os.listdir(os.path.join(origin, "SOURCES")))
+    """Find and copy patches from a dist-git repo to a source-git repo.
+
+    This looks for 'SOURCES/*.patch' in ORIGIN and copy everything found to
+    'centos-packaging/SOURCES/' in DEST.
+
+    After copying stage the changes in DEST.
+    """
+    files = filter(
+        lambda x: x.endswith(".patch"), os.listdir(os.path.join(origin, "SOURCES"))
+    )
     os.makedirs(os.path.join(dest, "centos-packaging", "SOURCES"), exist_ok=True)
     for f in files:
-        shutil.copy2(os.path.join(origin, "SOURCES", f), os.path.join(dest, "centos-packaging", "SOURCES", f))
+        shutil.copy2(
+            os.path.join(origin, "SOURCES", f),
+            os.path.join(dest, "centos-packaging", "SOURCES", f),
+        )
     ctx.invoke(stage, gitdir=dest)
 
 
 @cli.command()
 @click.argument("origin")
 @click.argument("dest")
+@log_call
 @click.pass_context
 def extract_archive(ctx, origin, dest):
-    import tempfile
-    import shutil
+    """Extract the source archive found in ORIGIN to DEST.
 
-    get_sources = sh.Command("get_sources.sh")
+    First, make sure that the archive was downloaded.
+
+    After extracting the archive, stage the changes in DEST.
+    """
+    # Make sure, the archive exists and use the STDOUT of get_sources.sh
+    # to find out its path.
     stdout = ""
     while "exists" not in stdout:
-        with sh.pushd(origin):
-            stdout = get_sources()
+        stdout = ctx.invoke(get_archive, gitdir=origin)
     archive = os.path.join(origin, stdout.partition(" exists")[0])
+
     with tempfile.TemporaryDirectory() as tmpdir:
         shutil.unpack_archive(archive, tmpdir)
+        # Expect an archive with a single directory.
         assert len(os.listdir(tmpdir)) == 1
         topdir = os.path.join(tmpdir, os.listdir(tmpdir)[0])
+        # These are all the files under the directory that was
+        # in the archive.
         files = os.listdir(topdir)
         for f in files:
             shutil.move(os.path.join(topdir, f), os.path.join(dest, f))
+
     ctx.invoke(stage, gitdir=dest)
 
 
 @cli.command()
 @click.argument("gitdir")
+@log_call
 @click.pass_context
 def apply_patches(ctx, gitdir):
-    """Apply the patches defined in the spec file"""
-    from rebasehelper.specfile import SpecFile
-    import re
+    """Apply the patches used in the SPEC-file found in GITDIR.
+
+    Apply all the patches used in the SPEC-file, then update the
+    SPEC-file by commenting the patches that were applied and deleting
+    those patches from the disk.
+    """
 
     class Specfile(SpecFile):
         def comment_patches(self, patch_indexes):
@@ -120,62 +222,79 @@ def apply_patches(ctx, gitdir):
                         package[i] = f"# {line}"
             self.spec_content.replace_section("%package", package)
 
-
+    specdir = os.path.join(gitdir, "centos-packaging", "SPECS")
+    specpath = os.path.join(specdir, get_local_specfile_path([specdir]))
+    logger.info(f"specpath = {specpath}")
     specfile = Specfile(
-        # TODO(csomh):
-        # - Take spec file from packit.yaml?
-        os.path.join(gitdir, "centos-packaging", "SPECS", "rpm.spec"),
-        sources_location=os.path.join(gitdir, "centos-packaging", "SOURCES"),
+        specpath, sources_location=os.path.join(gitdir, "centos-packaging", "SOURCES"),
     )
     repo = git.Repo(gitdir)
 
-    to_be_removed = []
-    delete = []
+    remove_from_spec = []
+    delete_patches = []
     for patch in specfile.get_applied_patches():
         logger.info(f"Apply Patch{patch.index}: {patch.get_patch_name()}")
         repo.git.apply(os.path.relpath(patch.path, gitdir), p=patch.strip)
-        delete.append(patch.path)
-        to_be_removed.append(patch.index)
+        delete_patches.append(patch.path)
+        remove_from_spec.append(patch.index)
 
-    logger.info(f"Number of patches to be commented: {len(to_be_removed)}")
+    logger.info(f"Number of patches to be commented: {len(remove_from_spec)}")
     # TODO(csomh):
-    # the bellow is not complete, as there are many more ways to apply
+    # the bellow is not complete, as there are many more ways to specify
     # patches in spec files. Cover this in the future.
-    specfile.comment_patches(to_be_removed)
-    specfile._process_patches(comment_out=to_be_removed)
+    specfile.comment_patches(remove_from_spec)
+    specfile._process_patches(comment_out=remove_from_spec)
     specfile.save()
-    for p in delete:
+    for p in delete_patches:
         os.unlink(p)
     ctx.invoke(stage, gitdir=gitdir)
 
-@cli.command()
-@click.argument("gitdir")
-def commit(gitdir):
-    repo = git.Repo(gitdir)
-    repo.git.commit(m="Import sources from dist-git")
 
 @cli.command()
 @click.argument("gitdir")
+@click.option("-m", default="Import sources from dist-git", help="Git commmit message")
+@log_call
+def commit(gitdir, m):
+    """Commit staged changes in GITDIR."""
+    repo = git.Repo(gitdir)
+    repo.git.commit(m=m)
+
+
+@cli.command()
+@click.argument("gitdir")
+@log_call
 def stage(gitdir):
+    """Stage all content in GITDIR."""
     repo = git.Repo(gitdir)
     repo.git.add(".")
+
 
 @cli.command()
 @click.argument("origin")
 @click.argument("dest")
+@log_call
 @click.pass_context
 def convert(ctx, origin, dest):
+    """Convert a dist-git repository into a source-git repository.
+
+    ORIGIN and DEST are in the format of
+
+        REPO_PATH:BRANCH
+
+    This command calls all the other commands.
+    """
     origin_dir, origin_branch = origin.split(":")
     dest_dir, dest_branch = dest.split(":")
 
     ctx.invoke(checkout, path=origin_dir, branch=origin_branch)
-    ctx.invoke(get_archive, path=origin_dir)
+    ctx.invoke(get_archive, gitdir=origin_dir)
     ctx.invoke(checkout, path=dest_dir, branch=dest_branch, orphan=True)
     ctx.invoke(extract_archive, origin=origin_dir, dest=dest_dir)
     ctx.invoke(copy_spec, origin=origin_dir, dest=dest_dir)
     ctx.invoke(copy_patches, origin=origin_dir, dest=dest_dir)
     ctx.invoke(apply_patches, gitdir=dest_dir)
     ctx.invoke(commit, gitdir=dest_dir)
+
 
 if __name__ == "__main__":
     cli()
