a6faac17a20c7e0fd60fec2b7e0a672575580251
Jiri Popelka <jpopelka@redhat.com>
2020-10-20 17:35:44 +0200

    Source-git onboarding scrips has been moved from centos_playground/
    to https://github.com/packit/source-git-onboarding

--- centos_playground/onboard/run-onboard.sh
+++
@@ -1,9 +0,0 @@
-mkdir --mode=0700 -p "${HOME}/.ssh"
-pushd "${HOME}/.ssh"
-install -m 0400 /my-ssh/id_rsa .
-install -m 0400 /my-ssh/id_rsa.pub .
-ssh-keyscan git.stg.centos.org >>known_hosts
-ssh-keyscan gitlab.com >>known_hosts
-popd
-
-python3 onboard.py
